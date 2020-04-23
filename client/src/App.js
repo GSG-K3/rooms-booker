1f2223af991889e62c47e8d4a399881694f78aaf
bayanseder <bayansider88@gmail.com>
2020-04-23 13:38:44 +0300

    create header

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+import Header from './components/Common/Header/Header'
 
 function App () {
   return (
     <Router>
       <switch>
-        <Route exact path="/"></Route>
+        <Route exact path="/">
+          <Header />
+        </Route>
         <Route exact path="/users">
           <div>hiiii user</div>
         </Route>
