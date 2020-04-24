522812b10d82390951944455a13f611939d50aaa
bayanseder <bayansider88@gmail.com>
2020-04-24 23:14:38 +0300

    last modify

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,15 @@
 import React from 'react'
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import Header from './components/Common/Header/Header'
-import UserHeader from './components/Layout/UserHome/UserHome'
+import UserHome from './components/Layout/UserHome/UserHome'
 
 function App () {
   return (
     <Router>
+      <Header />
       <switch>
-        <Route exact path="/" component={Header} />
-        <Route exact path="/home" component={UserHeader} />
+        {/* <Route exact path="/" component={Header} /> */}
+        <Route exact path="/home" component={UserHome} />
       </switch>
     </Router>
   )
