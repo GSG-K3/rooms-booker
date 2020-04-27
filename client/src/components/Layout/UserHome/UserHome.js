fbe5c90d6060ae29b24912ff3865ae07bcc3be12
bayanseder <bayansider88@gmail.com>
2020-04-28 00:03:54 +0300

    cheang css file name to camelcase

--- client/src/components/Layout/UserHome/UserHome.js
+++ client/src/components/Layout/UserHome/UserHome.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import Profile from '../../../Images/Ellipse 2.jpg'
 import Delete from '../../../Images/delete.jpg'
 import Edit from '../../../Images/Edit.png'
-import './UserHome.css'
+import './userHome.css'
 import axios from 'axios'
 class UserHome extends Component {
   state = {
@@ -17,20 +17,20 @@ class UserHome extends Component {
 
 
   render () {
-    return <div className='Component_continer'>
-      <div className='User_profile__div'>
+    return <div className='component_continer'>
+      <div className='user_profile__div'>
         <img src={Profile} />
         <h2>Bayan_Seder</h2>
       </div>
-      <div className='Events_continer__div'>
-        <h3 className='Events'>Your Events</h3>
+      <div className='events_continer__div'>
+        <h3 className='events'>Your Events</h3>
         {this.state.events.map((event)=>{
-      return  <div className='Event_card'>
-    <div className='Event_title'>
+      return  <div className='event_card'>
+    <div className='event_title'>
       <h3>{event.event_title}</h3>
       <p>{event.event_date}</p>
     </div>
-    <div className='Event_option'>
+    <div className='event_option'>
       <div>
       <img src={Delete} alt='delete' />
       </div>
@@ -41,7 +41,7 @@ class UserHome extends Component {
   </div>
     })}
       </div>
-      <button className='Back_button'>Back</button>
+      <button className='back_button'>Back</button>
     </div>
   }
 }
