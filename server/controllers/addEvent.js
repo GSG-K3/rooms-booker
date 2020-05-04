f0525e006e20be426880cc668f955608ffe9a3d8
bayanseder <bayansider88@gmail.com>
2020-05-04 14:51:46 +0300

    add simple validation to the form

--- server/controllers/addEvent.js
+++ server/controllers/addEvent.js
@@ -3,7 +3,7 @@ const eventPost = require('../database/queries/eventPost')
 const addEvent = (req, res) => {
   const data = req.body
   eventPost(data)
-  res.send('done')
+  res.send('an event added to the list')
 }
 
 module.exports = addEvent
