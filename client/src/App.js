9b97c4e7390670dec7e3ea29600b2fe6efae7135
yaqootturman <yaqoot.turman@gmail.com>
2020-05-06 02:14:05 +0300

    fix render events twice

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,16 @@
 import React from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import Rooms from './components/Layout/Rooms/Rooms.js'
 import Header from './components/Common/Header/Header'
-import Events from './components/Layout/Events/Events'
 import UserHome from './components/Layout/UserHome/UserHome'
 import NavBar from './components/Common/NavBar/NavBar.js'
 
-function App () {
+function App() {
   return (
     <Router>
       <Header />
-      <NavBar/>
       <Switch>
-        <Route exact path = '/' component={Events}/>
+        <Route exact path='/' component={NavBar} />
         <Route exact path="/home" component={UserHome} />
-        <Route exact path="/rooms" component={Rooms} />
       </Switch>
     </Router>
   )
