c5754ba79a81332e92728b31c750d164ca05a2e2
aboodswiti <swaiti393@gmail.com>
2020-05-06 02:01:09 +0300

    delete the comments

--- server/database/queries/getPassword.js
+++ server/database/queries/getPassword.js
@@ -1,19 +1,5 @@
 
 const dbconniction = require('../db_connection')
-/* process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0'
-const getPassword = (email, callBack) => {
-  const sql = {
-    text: 'SELECT password FROM users where email=$1',
-    values: [email]
-  }
-  dbconniction.query(sql, (err, resulte) => {
-    if (err) {
-      return callBack(err)
-    }
-    return callBack(null, resulte.rows[0])
-  })
-} */
-
 
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0'
 const getPassword = (email) => {
