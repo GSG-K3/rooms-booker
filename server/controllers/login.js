cd45c687b457fb21d6036be9312ae27d10a0e60c
aboodswiti <swaiti393@gmail.com>
2020-05-05 03:20:07 +0300

    delete comments

--- server/controllers/login.js
+++ server/controllers/login.js
@@ -46,17 +46,3 @@ exports.login = (req, res) => {
     }
   });
 };
-
-/* const login = (req, res) => {
-    console.log('req body :', req.body)
-    res.send(req.body)
-  }
-  
-  module.exports = login */
-
-/*   exports.login = (req, res) => {
-
-    res.send(req.body)
-    console.log('req body :', req.body)
-
-    } */
--- server/database/queries/getPassword.js
+++ server/database/queries/getPassword.js
@@ -1,14 +1,3 @@
-/* const dbConnection = require("../db_connection");
-
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0'
-
-const getPassword = (email) => {
-  return dbConnection
-    .query('SELECT password FROM users where email=$1', [email])
-    .then((res) => res.rows[0])
-    .catch((err) => console.log('error',err))
-};
- */
 
 const dbconniction = require('../db_connection')
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0'
