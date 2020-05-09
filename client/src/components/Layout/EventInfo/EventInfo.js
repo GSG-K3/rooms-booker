8c0ba353ea03c1932fd516a545d24d38d9422313
tasneembhiri <141059@ppu.edu.ps>
2020-05-09 23:18:02 +0300

    add mobile first style to the event information #25

--- client/src/components/Layout/EventInfo/EventInfo.js
+++ client/src/components/Layout/EventInfo/EventInfo.js
@@ -1,16 +1,37 @@
 import React from 'react'
-
+import './eventInfo.css'
 const EventInfo = (props) => {
   const data = sessionStorage.getItem('event')
   const eventInfo = JSON.parse(data)
 
   return (
-    <div>
-      <p> {eventInfo.event_title} </p>
-      <p> {eventInfo.event_description}</p>
-      <p> {eventInfo.event_author}</p>
-      <p> {eventInfo.event_date}</p>
-      <p> {eventInfo.event_note}</p>
+    <div className='eventinfo'>
+      <div>
+        <h2 className ='eventinfo__title'> {eventInfo.event_title} </h2>
+      </div>
+      <div className='event_info'>
+
+        <div className ='event_info__description'>
+          <h2>Description About The Event:</h2>
+          <p> {eventInfo.event_description}</p>
+        </div>
+
+        <div className ='event_info__author'>
+          <h2>Event Author:</h2>
+          <p> {eventInfo.event_author}</p>
+        </div>
+
+        <div className ='event_info__date'>
+          <h2>Event Date:</h2>
+          <p> {eventInfo.event_date}</p>
+        </div>
+
+        <div className ='event_info__note'>
+          <h2>Notes:</h2>
+          <p> {eventInfo.event_note}</p>
+        </div>
+
+      </div>
     </div>
   )
 }
