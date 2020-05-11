1e16e3bbfbfb9baf320dfc1639b7e01adc80cfae
Hanan795 <hawawdeh95@gmail.com>
2020-05-11 04:27:39 +0300

    edit  #32

--- server/database/queries/getAvailableRooms.js
+++ server/database/queries/getAvailableRooms.js
@@ -4,10 +4,13 @@ const availableRooms = (eventDate) => {
   const sql = {
     text:
       'SELECT room_name FROM rooms WHERE room_id NOT IN (SELECT room_id FROM events WHERE event_date = $1)',
-    values: [eventDate],
+    values: [eventDate]
   }
 
-  return dbconnection.query(sql)
+  return dbconnection
+    .query(sql)
+    .then(res => res.rows)
+    .catch(err => err)
 }
 
 module.exports = availableRooms
