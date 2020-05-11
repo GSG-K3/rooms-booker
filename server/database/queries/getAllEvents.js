f15941b1df3953e2fefc0e96200ac236d03e5e33
bayanseder <bayansider88@gmail.com>
2020-05-11 15:10:46 +0300

    delete promise from queries

--- server/database/queries/getAllEvents.js
+++ server/database/queries/getAllEvents.js
@@ -4,7 +4,5 @@ process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0'
 const events = () => {
   return dbconnection
     .query('select events.event_title, events.event_date from events')
-    .then(res => res.rows)
-    .catch(err => console.log(err))
 }
 module.exports = events
--- server/database/queries/getUserHome.js
+++ server/database/queries/getUserHome.js
@@ -1,16 +1,11 @@
 const dbconniction = require('../db_connection')
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0'
-const userHome = (userId, callBack) => {
+const userHome = (userId) => {
   const sql = {
     text: 'SELECT * FROM events WHERE user_id =$1',
     values: [userId]
   }
-  dbconniction.query(sql, (err, resulte) => {
-    if (err) {
-      return callBack(err)
-    }
-    return callBack(null, resulte.rows)
-  })
+  dbconniction.query(sql)
 }
 
 module.exports = userHome
