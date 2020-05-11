93ed598c4575a44eddc479571789babdf00a0566
Hanan795 <hawawdeh95@gmail.com>
2020-05-11 03:35:56 +0300

    final edits  #32

--- server/controllers/getAvailableRooms.js
+++ server/controllers/getAvailableRooms.js
@@ -2,7 +2,6 @@ const getAvailableRooms = require('../database/queries/getAvailableRooms')
 
 const getAvailableRoom = (req, res) => {
   const eventDate = req.query.date
-  console.log(eventDate)
   getAvailableRooms(eventDate)
     .then(data => res.json(data))
     .catch(err => err)
--- server/database/queries/getAvailableRooms.js
+++ server/database/queries/getAvailableRooms.js
@@ -1,16 +1,17 @@
 const dbconnection = require('../db_connection')
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0'
 
-const availableRooms = (eventDate) => {
+const availableRooms = (eventDate, cb) => {
   const sql = {
     text: 'SELECT room_name FROM rooms WHERE room_id NOT IN (SELECT room_id FROM events WHERE event_date = $1)',
     values: [eventDate]
   }
 
-  return dbconnection
-    .query(sql)
-    .then(res => res.rows)
-    .catch(err => err)
+  dbconnection.query(sql, (err, res) => {
+    if (err) {
+      return cb(err)
+    }
+    return cb(null, res.rows)
+  })
 }
 
 module.exports = availableRooms
