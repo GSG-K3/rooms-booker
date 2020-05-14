ac65921e1bd1c0c2ed35d4b3639e61dde64737e7
bayanseder <bayansider88@gmail.com>
2020-05-15 02:36:00 +0300

    change events cards style

--- client/src/components/Layout/Event/Event.js
+++ client/src/components/Layout/Event/Event.js
@@ -10,10 +10,7 @@ class Event extends Component {
     const { event_title , event_date } = this.props
     return (
       <div  onClick={this.showInfo}>
-        <div className="event_card">
-          <div className='color_div_yellow'></div>
-          <div className='color_div_blue'></div>
-          <div className='color_div_lightBlue'></div>
+        <div className="event_card_event_component">
           <div className='event_title_div'>
         <h2>{event_title}</h2>
         <p>{event_date.slice(0,10)}</p>
