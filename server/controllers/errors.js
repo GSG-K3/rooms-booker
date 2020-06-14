39dffc2a9279d34d270df7c987ec54a5d4e11eec
bayanseder <bayansider88@gmail.com>
2020-06-14 12:16:58 +0300

    add middel wear to server err

--- server/controllers/errors.js
+++ server/controllers/errors.js
@@ -2,8 +2,9 @@ const notFound = (req, res) => {
   res.status(404).send({ message: '404  PAGE NOT FOUND' })
 }
 
-const serverErr = (req, res) => {
+const serverErr = (err, req, res, next) => {
   res.status(500).send({ message: '500  SERVER ERROR' })
+  next()
 }
 
 module.exports = { notFound, serverErr }
