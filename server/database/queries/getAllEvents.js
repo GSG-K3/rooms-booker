bc86141f341a788637f01e743fdb03d81264dab0
bayanseder <bayansider88@gmail.com>
2020-06-15 13:31:24 +0300

    stop rendering past events

--- server/database/queries/getAllEvents.js
+++ server/database/queries/getAllEvents.js
@@ -1,7 +1,11 @@
 
 const dbconnection = require('../db_connection')
 const events = () => {
+  const sql = {
+    text: 'select * from events inner join rooms on events.room_id = rooms.room_id where event_date >= $1 ',
+    value: [new Date().toLocaleDateString()]
+  }
   return dbconnection
-    .query('select * from events inner join rooms on events.room_id = rooms.room_id')
+    .query(sql.text, sql.value)
 }
 module.exports = events
