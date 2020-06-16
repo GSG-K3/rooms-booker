4fa55ec8e8544dc6ff8392ba6ba190ec60904242
tasneembhiri <141059@ppu.edu.ps>
2020-06-16 14:28:43 +0300

    add userId to endpoint for homepage #119

--- client/src/components/Common/Header/Header.js
+++ client/src/components/Common/Header/Header.js
@@ -1,19 +1,45 @@
-import React from 'react'
+import React, { Component } from 'react'
 import GSGlog from '../../../Images/gsg_logo.jpg'
 import Profile from '../../../Images/Ellipse 2.jpg'
 import './Header.css'
 import { Link } from 'react-router-dom'
+import { withRouter } from 'react-router'
+import axios from 'axios'
 
-function Header () {
-  return (
-    <div className='header'>
-      <Link to={'/'} className='header_link'>
-        <img className='logo' src={GSGlog} alt='logo' />
-      </Link>
-      <Link to = {'/home'}>
-        <img className='profile' src={Profile} alt='profile'/></Link>
-    </div>
-  )
+class Header extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      email: '',
+      userId: '',
+      userName: ''
+    }
+  }
+
+  show = ()=> {
+    const { history } = this.props
+    axios.get('/api/check').then(({ data }) => {
+      const { success, email, userId, userName } = data
+      if (success) {
+        this.setState({  email, userId, userName } , () => {
+          history.push(`/home/${userId}`)
+        })
+      } else {
+        return history.push('/login')
+      }
+    })
+  }
+
+  render () {
+    const { userId } = this.state 
+    return (
+      <div className='header'>
+        <Link to={'/'} className='header_link'>
+          <img className='logo' src={GSGlog} alt='logo' />
+        </Link>
+        <Link to = {{pathname:`/home/${userId}`}}>
+          <img className='profile' src={Profile} alt='profile' onClick={this.show}/></Link></div>
+    )
+  }
 }
-
-export default Header
+export default withRouter(Header)
