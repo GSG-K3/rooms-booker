5ed17793a0299db70af9c362df33c05944273675
tasneembhiri <141059@ppu.edu.ps>
2020-06-22 18:44:56 +0300

    add end date #148

--- server/controllers/login.js
+++ server/controllers/login.js
@@ -13,7 +13,7 @@ exports.login = (req, res) => {
   if (!email || !password) {
     res.status(400).json({ message: 'All Fields are Required', status: 400 })
   }
-
+// get the email password from database 
   getPassword(email)
     .then((result) => {
       if (!result || result.rows.length === 0) {
@@ -21,14 +21,14 @@ exports.login = (req, res) => {
       }
 
       const hash = result.rows[0].password
-
+// check if the password matched 
       bcrypt.compare(password, hash, (err, result2) => {
         if (err) {
           return res.json({ err })
         }
         if (result2) {
           const { user_id, user_name } = result.rows[0]
-
+// create token and cookie
           const token = createToken(email, user_id, user_name, SECRET)
           return res
             .cookie('token', token, { maxAge: 900000, httpOnly: true })
