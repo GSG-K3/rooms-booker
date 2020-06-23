630c76d568558d818335a160c2febf90f74eb386
Hanan795 <hawawdeh95@gmail.com>
2020-06-23 17:37:37 +0300

    solve enviroment variable problem #140

--- client/src/components/Layout/GoogleCalendar/Calender.js
+++ client/src/components/Layout/GoogleCalendar/Calender.js
@@ -1,19 +1,18 @@
-import React from 'react'
 // const { CLIENT_ID, API_KEY, DISCOVERY_DOCS, SCOPES } = process.env
 const gapi = window.gapi
-const CLIENT_ID = '558072145685-hl8laqhvbqcd0f5vfrnt61v1ts1ls5lh.apps.googleusercontent.com'
-const API_KEY = 'AIzaSyBaXfE46xxRXpcw7vSUmIZaKlwJrec1bGY'
-const DISCOVERY_DOCS = ['https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest']
-const SCOPES = 'https://www.googleapis.com/auth/calendar'
+const { REACT_APP_CLIENT_ID } = process.env
+const { REACT_APP_API_KEY } = process.env
+const { REACT_APP_DISCOVERY_DOCS } = process.env
+const { REACT_APP_SCOPES } = process.env
 
 const CalenderApi = (event) => {
   // make authontication by google account
   gapi.load('client:auth2', () => {
     gapi.client.init({
-      apiKey: API_KEY,
-      clientId: CLIENT_ID,
-      discoveryDocs: DISCOVERY_DOCS,
-      scope: SCOPES
+      apiKey: REACT_APP_API_KEY,
+      clientId: REACT_APP_CLIENT_ID,
+      discoveryDocs: REACT_APP_DISCOVERY_DOCS,
+      scope: REACT_APP_SCOPES
     })
     gapi.client.load('calendar', 'v3')
     // check if user is login
